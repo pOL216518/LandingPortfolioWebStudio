cf1f89e47e0c87ca9f508a648b67bb20610488cb
Ta1ant <149287822+pOL216518@users.noreply.github.com>
2024-12-25 22:44:40 +0800

    second com.v2

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-  { name: "Блог", href: "/projects" },
+  { name: "Блог", href: "/blog" },
   { name: "Контакты", href: "/contact" },
 ];
 
